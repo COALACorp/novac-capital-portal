c2f708fd276f8a7467ccf7b71d2d6067be45a044
Mario Orellana <mario@journeyhero.net>
2023-06-05 21:05:28 -0600

    Setup config for github pages deployment

--- novac-capital-portal/vite.config.ts
+++ novac-capital-portal/vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: "/novac-capital-portal/",
-    build: {
-        target: "esnext",
-    },
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: "/",
+        build: {
+            target: "esnext",
+        }
+    };
+
+    if (command !== "serve")
+        config.base = "/novac-capital-portal/";
+
+    return config;
 });
