8e459e0f3cb551f1033f170ff9d2c8fc30a845bd
Mario Orellana <mario@journeyhero.net>
2023-06-30 11:48:30 -0600

    Implement environment variables

--- novac-capital-portal/src/utils/firebase.ts
+++ novac-capital-portal/src/utils/firebase.ts
@@ -8,14 +8,14 @@ import { getAuth, GoogleAuthProvider, signOut } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCeWjQQPBbZXBpb8xC830IPSWgpWf5wq4Q",
-    authDomain: "novaccapital-65e2a.firebaseapp.com",
-    projectId: "novaccapital-65e2a",
-    storageBucket: "novaccapital-65e2a.appspot.com",
-    messagingSenderId: "233323889630",
-    appId: "1:233323889630:web:598bf620c3dbb77d01934f",
-    measurementId: "G-60WY5KFSTZ",
-    databaseURL: "https://novaccapital-65e2a-default-rtdb.firebaseio.com",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_COMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
