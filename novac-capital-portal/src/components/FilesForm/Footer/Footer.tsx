f69d069321c73c3afea14abdb50a3c9116b7b07e
Mario Orellana <mario@journeyhero.net>
2023-07-14 14:51:21 -0600

    Add Footer component in checklist

---
+++ novac-capital-portal/src/components/FilesForm/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import "@/styles/filesform.css";
+import Box from "@mui/material/Box";
+
+import Contact from "./Contact";
+import WhatsAppButton from "./WhatsAppButton";
+
+function Footer() {
+    return (
+        <Box id="footer">
+            <p>Si tienes alguna duda te puedes comunicar con nosotros de las siguientes maneras:</p>
+            <Contact />
+            <WhatsAppButton />
+        </Box>
+    );
+}
+
+export default Footer;
