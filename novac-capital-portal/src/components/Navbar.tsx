4557a03d51790f54913e3348354f85527ee34ecf
Mario Orellana <mario@journeyhero.net>
2023-08-30 21:51:12 -0600

    Add sign out link in navbar

--- novac-capital-portal/src/components/Navbar.tsx
+++ novac-capital-portal/src/components/Navbar.tsx
@@ -4,10 +4,15 @@ import Link from "next/link";
 
 import { useAppSelector } from "@/app/hooks";
 import { selectUser } from "@/features/user/userSlice";
+import { SignOut } from "@/utils/firebase";
 
 function Navbar() {
     const router = useRouter();
     const user = useAppSelector(selectUser);
+
+    const handleSignOut = () => {
+        SignOut(() => router.push("/"));
+    };
 
     return (
         <div id="navbar">
@@ -25,10 +30,13 @@ function Navbar() {
                     user.admin ? (
                         <Link className="navbar-link" href="/admin_portal">Dashboard</Link>
                     ) : (
-                        <Link className="navbar-link" href="/files_form">Mi Cuenta</Link>
+                        <>
+                            <Link className="navbar-link" href="/files_form">Mi Cuenta</Link>
+                            <a className="navbar-link" onClick={handleSignOut}>Cerrar Sesión</a>
+                        </>
                     )
                 ) : (
-                    <Link className="navbar-link" href="/signin">Log In</Link>
+                    <Link className="navbar-link" href="/signin">Iniciar Sesión</Link>
                 )}
             </div>
         </div>
